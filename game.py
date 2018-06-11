50c76ccf9198af037a042079bc38293502df4ce7
DANIEL TAPP <danieltapp@DANIELs-MacBook-Pro.local>
2018-06-11 14:43:52 -0400

    Broke up web scraper and game engine into seperate module. Have scraped data pushed to CSV file

--- game.py
+++ game.py
@@ -1,3 +1,57 @@
-class Game:
-    pass
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+from csv import DictReader
+
+base_url = 'http://quotes.toscrape.com'
+
+def read_quotes(filename):
+    with open(filename, 'r') as file:
+        csv_reader = DictReader(file)
+        return list(csv_reader)
+
+quotes = read_quotes('quotes.csv')
+
+
+def game(quotes):
+    quote = choice(quotes)
+    remaining_guesses = 4
+    print("Here's a quote : ")
+    print(quote['text'])
+    guess = ''
+    while guess.lower() != quote['author'].lower() and remaining_guesses > 0:
+        guess = input(f'Who said this? Guesses remaining : {remaining_guesses}. \n').lower()
+        if guess == quote['author'].lower():
+            print('you win!!!')
+            break
+        remaining_guesses -= 1
+        if remaining_guesses == 3:
+            res = requests.get(f"{base_url}{quote['url']}")
+            soup = BeautifulSoup(res.text, 'html.parser')
+            born_date = soup.find(class_="author-born-date").get_text()
+            born_location = soup.find(class_="author-born-location").get_text()
+            print(f"Here's a hint : The author was born on {born_date} {born_location}")
+        elif remaining_guesses == 2:
+            print(f"The author's first name start's with {quote['author'][0]}")
+        elif remaining_guesses == 1:
+            print(f"The author's last name start's with {quote['author'].split(' ')[1][0]}")
+        else:
+            print(f"Sorry, you ran out of guesses. The answer is : {quote['author']}")
+    again = ''
+    while again not in ('y', 'yes', 'n', 'no'):
+        again = input("Play again? (y / n)")
+    if again.lower() in ('y', 'yes'):
+        return game(quotes)
+    else:
+        print('Goodbye')
+
+game(quotes)
+
+
+
+
+
+
+
+
 
--- quote_scrape.py
+++ quote_scrape.py
@@ -1,18 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
-from csv import writer
+from csv import DictWriter
+from time import sleep
 
-response = requests.get('http://quotes.toscrape.com')
-soup = BeautifulSoup(response.text, 'html.parser')
-quotes = soup.find_all(class_="quote")
 
-with open('quotes.csv', 'w') as csv_file:
-    csv_writer = writer(csv_file)
-    csv_writer.writerow(['quote', 'author', 'url'])
+
+base_url = 'http://quotes.toscrape.com'
+
+def scrape_quotes():
+    all_quotes = []
+    page = '/page/1'
+    while page:
+        response = requests.get(f'{base_url}{page}')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        quotes = soup.find_all(class_="quote")
+        for quote in quotes:
+            all_quotes.append({
+                'text' : quote.find(class_='text').get_text(),
+                'author' : quote.find(class_='author').get_text(),
+                'url' : quote.find('a').attrs['href']
+            })
+        next = soup.find(class_="next")
+        page = next.find("a")['href'] if next else None
+        sleep(1)
+    return all_quotes
+
+
+quotes = scrape_quotes()
+
+with open('quotes.csv', 'w') as file:
+    headers = ['text', 'author', 'url']
+    csv_writer = DictWriter(file, fieldnames=headers)
+    csv_writer.writeheader()
     for quote in quotes:
-        text = quote.find(class_='text').get_text()
-        author = quote.find(class_='author').get_text()
-        url = quote.find('a').attrs['href']
-        csv_writer.writerow([text, author, url])
+        csv_writer.writerow(quote)
+
+
+
+
+
+
+
+
 
 
